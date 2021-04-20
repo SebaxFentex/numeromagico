4756f7072947d704ef55bfcd3ee67450395ce75f
SebaxFentex <sebaxfentex1@gmail.com>
2021-04-20 16:25:40 -0500

    actualizaciones

--- numeromagico.js
+++ numeromagico.js
@@ -15,12 +15,14 @@ let numeroAleatorio = Math.floor(Math.random() * 1000) + 1;
             document.getElementById("texto").innerHTML = "GANASTE";
         }else if (i === 10) {
             
-            document.getElementById("texto").innerHTML = "PERDISTE";
+            document.getElementById("texto").innerHTML = "PERDISTE, el numero era " + numeroAleatorio;
+        }else if (i > 10){
+
         }else {
             if(numeroIngresado < numeroAleatorio) {
-                document.getElementById("texto").innerHTML = "Muy Bajo!";
+                document.getElementById("texto").innerHTML = "Muy Bajo!" + " te quedan " + (10-i).toString() + " intentos";
             }else if(numeroIngresado > numeroAleatorio) {
-                document.getElementById("texto").innerHTML = "Muy alto!";
+                document.getElementById("texto").innerHTML = "Muy alto!" + " te quedan " + (10-i).toString() + " intentos";
             }
         }
 
@@ -28,3 +30,26 @@ let numeroAleatorio = Math.floor(Math.random() * 1000) + 1;
         campoingreso.value = '';
         campoingreso.focus();
     }
+
+    function hora(){
+        var fecha = new Date;
+        var hora = fecha.getHours();
+        var minuto = fecha.getMinutes();
+        var segundos = fecha.getSeconds();
+
+        if(minuto < 10){
+            minuto = "0" + minuto;
+        }
+
+        if(segundos < 10){
+            segundos = "0" + segundos;
+        }
+
+        var horacompleta = hora + ":" + minuto + ":" + segundos;
+        document.getElementById("hora").innerHTML = horacompleta;
+
+    }
+
+    var intervalId = window.setInterval(function(){
+        hora();
+    }, 1000);
