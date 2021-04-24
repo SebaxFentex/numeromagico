52135af791ff1df015ced901e26142ff744382d7
SebaxFentex <sebaxfentex1@gmail.com>
2021-04-23 22:17:38 -0500

    actualizacion tabla de posiciones

--- numeromagico.js
+++ numeromagico.js
@@ -3,7 +3,68 @@ let numeroAleatorio = Math.floor(Math.random() * 1000) + 1;
     const texto = document.querySelector('.texto');
     const numeroIngresado = document.querySelector('.campoingreso');
 
-    let i = 1;
+    // LIMPIAR LOCALSTORAGE
+    //localStorage.clear();
+
+    //MOSTRAR PODIO
+    //alert("PODIO:\n1: " + localStorage.getItem("nombretop1") + ", " + localStorage.getItem("puntajetop1") + " puntos, el" + localStorage.getItem("horatop1") + "\n2: " + localStorage.getItem("nombretop2") + ", " + localStorage.getItem("puntajetop2") + " puntos, el" + localStorage.getItem("horatop2") + "\n3: " + localStorage.getItem("nombretop3") + ", " + localStorage.getItem("puntajetop3") + " puntos, el" + localStorage.getItem("horatop3"));
+
+    let i = 1; 
+
+    function guardarPuntaje(nombre, puntaje, horayfecha){
+
+        // UBICA PUNTAJE EN EL TOP
+        var puntajetop1 = localStorage.getItem("puntajetop1");
+        var puntajetop2 = localStorage.getItem("puntajetop2");
+        var puntajetop3 = localStorage.getItem("puntajetop3");
+
+        if(puntajetop1 === null){
+            escribirPuntaje(1, nombre, puntaje, horayfecha);
+        }
+        else{
+            if(puntajetop1 > puntaje){
+                moverPuntaje(2,3);
+                moverPuntaje(1,2);
+                escribirPuntaje(1, nombre, puntaje, horayfecha);
+            }
+            else{
+                if(puntajetop2 === null){
+                    escribirPuntaje(2, nombre, puntaje, horayfecha);
+                }
+                else{
+                    if(puntajetop2 > puntaje){
+                        moverPuntaje(2,3);
+                        escribirPuntaje(2, nombre, puntaje, horayfecha);
+                    }
+                    else{
+                        if(puntajetop3 === null){
+                            escribirPuntaje(3, nombre, puntaje, horayfecha);
+                        }
+                        else{
+                            if(puntajetop3 > puntaje){
+                                escribirPuntaje(3, nombre, puntaje, horayfecha);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        location.reload();
+    }
+
+    function moverPuntaje(desde, hasta){
+
+        localStorage.setItem("nombretop" + hasta, localStorage.getItem("nombretop" + desde));
+        localStorage.setItem("puntajetop" + hasta, localStorage.getItem("puntajetop" + desde));
+        localStorage.setItem("horatop" + hasta, localStorage.getItem("horatop" + desde));
+        
+    }
+
+    function escribirPuntaje(posicion, nombre, puntaje, hora){
+        localStorage.setItem("nombretop" + posicion, nombre);
+        localStorage.setItem("puntajetop" + posicion, puntaje);
+        localStorage.setItem("horatop" + posicion, hora);
+    }
 
     function revisar() {
         console.log(numeroAleatorio);
@@ -13,6 +74,9 @@ let numeroAleatorio = Math.floor(Math.random() * 1000) + 1;
         }
         if (numeroIngresado === numeroAleatorio) {
             document.getElementById("texto").innerHTML = "GANASTE";
+            var nombre = prompt("Ingresa tu nombre para guardar tu puntaje", "Invitado");
+            guardarPuntaje(nombre, i, new Date);
+            alert("PODIO:\n1: " + localStorage.getItem("nombretop1") + ", " + localStorage.getItem("puntajetop1") + " puntos, el " + localStorage.getItem("horatop1") + "\n2: " + localStorage.getItem("nombretop2") + ", " + localStorage.getItem("puntajetop2") + " puntos, el " + localStorage.getItem("horatop2") + "\n3: " + localStorage.getItem("nombretop3") + ", " + localStorage.getItem("puntajetop3") + " puntos, el " + localStorage.getItem("horatop3"));
         }else if (i === 10) {
             
             document.getElementById("texto").innerHTML = "PERDISTE, el numero era " + numeroAleatorio;
